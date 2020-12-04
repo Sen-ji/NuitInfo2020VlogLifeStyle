275445310a13a70f2bc5a84fa598eb2e421ef551
Alexaline Thomas <thomas.alexaline@etu.u-bordeaux.fr>
2020-12-04 04:40:28 +0100

    leet speak fonctionnel

--- leet_speak/index.js
+++ leet_speak/index.js
@@ -1,12 +1,12 @@
 var leet = {
 	"level_1" : {
-		"a":"4", "b":"8", "e":"3", "g":"6", "i":"1", "l":"1", "o":"0", "r":"2", "s":"5", "t":"7", "z":"2"
+		"a":"4", "b":"8", "e":"3", "g":"6", "i":"1", "l":"£", "o":"0", "r":"2", "s":"5", "t":"7", "z":"2"
 	},
 	"level_2" : {
-		"a":"4", "b":"8", "c":"(", "d":"[)", "e":"3", "f":"|=", "g":"6", "h":"|-|", "i":"1", "j":"_|", "k":"X", "l":"1", "m":"|\\/|", "n":"|\\|", "o":"0", "p":"|*", "q":"(_,)", "r":"2", "s":"5", "t":"7", "u":"(_)", "v":"\\/", "w":"\\/\\/", "x":"><", "y":"7", "z":"2"
+		"a":"4", "u":"(_)", "b":"8", "c":"(", "d":"[)", "e":"3", "f":"|=", "g":"6", "h":"|-|", "i":"1", "j":"_|", "k":"|{", "l":"£", "m":"|\\/|", "n":"|\\|", "o":"0", "p":"|*", "q":"(_,)", "r":"2", "s":"5", "t":"7", "v":"\\/", "w":"\\/\\/", "x":"><", "y":"7", "z":"2"
 	},
 	"level_3" : {
-		"a":"/-\\", "b":"|3", "c":"(", "d":"|)", "e":"[-", "f":"|=", "g":"(_+", "h":"#", "i":"!", "j":"_|", "k":"|<", "l":"|_", "m":"/\\/\\", "n":"|\\|", "o":"()", "p":"|>", "q":"(_,)", "r":"/2", "s":"$", "t":"¯|¯", "u":"|_|", "v":"|/", "w":"\\|/", "x":")(", "y":"j", "z":"7_"
+		"k":"|\\\“", "a":"/-\\", "n":"|\\|", "g":"(_+", "q":"(_,)", "o":"()", "x":")(", "u":"|_|", "b":"|3", "c":"(", "d":"|)", "e":"[-", "f":"|=", "h":"#", "i":"!", "j":"_|", "l":"|_", "m":"/\\/\\", "p":"|°", "r":"/2", "s":"$", "t":"¯|¯", "v":"|/", "w":"\\|/", "z":"7_", "y":"7"
 	}
 };
 
@@ -18,14 +18,33 @@ function convert(input, map) {
 	return result;
 }
 
-function process(level) {
+function reverse(input, map) {
+	let result = input;
+	Object.keys(map).forEach(function(k){
+		result = result.replaceAll(map[k], k);
+	});
+	return result;
+}
+
+function processBack() {
 	let doms = document.querySelectorAll(".leet");
-	for (let dom of doms) {
-		console.log(dom);
-		dom.innerHTML = convert(dom.innerHTML.toLowerCase(), leet["level_"+level]);
+	for(let dom of doms) {
+		//console.log(dom);
+		dom.innerHTML = reverse(dom.innerHTML.toLowerCase(), leet["level_" + level]);
 	}
 }
 
+function process() {
+	if(level > 0) {
+		let doms = document.querySelectorAll(".leet");
+		for (let dom of doms) {
+			//console.log(dom);
+			dom.innerHTML = convert(dom.innerHTML.toLowerCase(), leet["level_"+level]);
+		}
+	}
+}
+
+var level = localStorage.getItem('level') || 0;
 var keys_konami = [];
 var konami = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"];
 var keys_covid = [];
@@ -34,8 +53,9 @@ var keys_neo = [];
 var neo = ["n", "e", "o"];
 
 document.addEventListener("keydown", event => {
+	let update_level = false;
 	let key_value = event.key.toLowerCase();
-	if (key_value == konami[keys_konami.length]) {
+	if (key_value == konami[keys_konami.length].toLowerCase()) {
 		keys_konami.push(key_value);
 	} else {
 		keys_konami = [];
@@ -51,34 +71,39 @@ document.addEventListener("keydown", event => {
 		keys_neo = [];
 	}
 	if (keys_konami.length == konami.length) {
-		process(1);
+		if(level == 0) {
+			level = 1;
+			process();
+			update_level = true;
+		}
 		resetInput();
 	}
 	if (keys_covid.length == covid.length) {
-		process(2);
+		if(level > 0) {
+			processBack();
+			level = 0;
+			update_level = true;
+		}
 		resetInput();
 	}
 	if(keys_neo.length == neo.length) {
-		process(3);
+		if(level > 0 && level < 3) {
+			processBack();
+			level = Number(level) + 1;
+			process();
+			update_level = true;
+		}
 		resetInput();
 	}
-	console.log(event.key);
-	if (event.key == " ") {
-		// console.log(reverse(document.getElementById("leet").innerHTML, leet["level_2"]));
-		document.location.reload();
+	if(update_level) {
+		localStorage.setItem('level', level);
+		console.log("level = " + level);
 	}
+	//console.log(event.key);
 });
 
 function resetInput() {
 	keys_covid = [];
 	keys_konami = [];
 	keys_neo = [];
-}
-
-function reverse(input, map) {
-	let result = input;
-	Object.keys(map).forEach(function(k){
-		result = result.replaceAll(map[k], k);
-	});
-	return result;
-}
+};
